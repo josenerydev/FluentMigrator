8ba302f41ae6b761939d5bb0d529c4f96033424a
José Nery <josenerydev@gmail.com>
2020-01-12 21:58:32 -0300

    Add project files.

---
+++ FluentMigrator.Sample/Migrations/02_PrimaryEmailColumnRename.cs
@@ -0,0 +1,17 @@
+﻿namespace FluentMigrator.Sample.Migrations
+{
+    [Migration(2)]
+    public class PrimaryEmailColumnRename : Migration
+    {
+        public override void Up()
+        {
+            Rename.Column("PrimaryEmail").OnTable("User").InSchema("dbo")
+                .To("Email");
+        }
+        public override void Down()
+        {
+            Rename.Column("Email").OnTable("User").InSchema("dbo")
+                .To("PrimaryEmail");
+        }
+    }
+}
---
+++ FluentMigrator.Sample/Migrations/06_AddEmailTo_sp_SelectUsers.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentMigrator.Sample.Migrations
+{
+    [Migration(6)]
+    class AddEmailTo_sp_SelectUsers : Migration
+    {
+        public override void Up()
+        {
+            Execute.Script(@"FluentMigrator.Sample\StoredProcedures\sp_SelectUsers.sql");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
